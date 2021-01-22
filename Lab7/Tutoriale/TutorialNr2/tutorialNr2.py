f482cd681f3551a440d9a1f42ab00836d6f30388
DanielSoltysiak <72917460+DanielSoltysiak@users.noreply.github.com>
2021-01-22 12:07:41 +0100

    Tutorial Nr 2

---
+++ Lab7/Tutoriale/TutorialNr2/tutorialNr2.py
@@ -0,0 +1,24 @@
+from redis import Redis
+
+redis_connection = Redis(decode_responses=True)
+
+key ="some-key"
+value ="some-val"
+
+redis_connection.set(key, value)
+print(redis_connection.get(key))
+
+redis_connection.append(key,"-append")
+print(redis_connection.get(key))
+
+redis_connection.delete(key)
+print(redis_connection.get(key))
+
+value =55
+
+redis_connection.set(key, value)
+print(redis_connection.get(key))
+
+print(redis_connection.incr(key,5))
+
+print(redis_connection.decr(key,20))
